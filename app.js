b0dc934dfde2f2dc25d2c8d53d0f1b08a2e0a175
soumarcelino <marcelinomatiasdesouza@gmail.com>
2020-06-21 18:15:42 -0300

    add first init

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
