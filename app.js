0538dfdb1d50df4bfdb633f0c9267603ababcefd
soumarcelino <marcelinomatiasdesouza@gmail.com>
2020-06-21 19:42:28 -0300

    add node cache

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const PORT = process.env.PORT || 5000;
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
+const NodeCache = require('node-cache');
+
+const oneDayInSeconds = 60 * 60 * 24;
+const VOA_URL = 'https://voaa.me/pages/campanhas-em-destaque';
+
+const cache = new NodeCache({
+  stdTTL: oneDayInSeconds,
+  checkperiod: oneDayInSeconds,
+});
+
 async function fetchData() {
-  const res = await fetch('https://voaa.me/pages/campanhas-em-destaque');
+  const res = await fetch(VOA_URL);
   const resText = await res.text();
   return parseRawText(resText);
 }
@@ -21,12 +31,20 @@ function parseRawText(text) {
       return {title, link, description};
     })
     .get();
+
+  cache.set('list', data);
   return data;
 }
 
 app.get('/', async function (req, res) {
-  const data = await fetchData()
-  res.send(data)
+  const cacheList = cache.get('list');
+
+  if (cacheList) {
+    res.send(cacheList);
+  } else {
+    const data = await fetchData();
+    res.send(data);
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
